8d221a0d22eecf923a5554e451887306c3b503b4
kmr0877 <kmr0877@gmail.com>
2017-11-15 13:07:03 +1100

    updated generalized version

---
+++ neural_Qtrain_gen.py
@@ -0,0 +1,65 @@
+import sys
+import gym
+import tensorflow as tf
+
+def init(env, env_name):
+    """
+    Initialise any globals, e.g. the replay_buffer, epsilon, etc.
+    return:
+        state_dim: The length of the state vector for the env
+        action_dim: The length of the action space, i.e. the number of actions
+
+    NB: for discrete action envs such as the cartpole and mountain car, this
+    function can be left unchanged.
+    with continuous action spaces, e.g. "Pendulum-v0"
+    1) you'll need to modify this function to discretise the action space and
+    create a global dictionary mapping from action index to action (which you
+    can use in `get_env_action()`)
+    2) for Pendulum-v0 `env.action_space.low[0]` and `env.action_space.high[0]`
+    are the limits of the action space.
+    3) setting a global flag iscontinuous which you can use in `get_env_action()`
+    might help in using the same code for discrete and (discretised) continuous
+    action spaces
+    """
+
+def create_q_model(state_dim, state_in, num_classes,name='None'):
+    with tf.variable_scope(name) as scope:
+
+
+    q_vars = {var.name[len(scope.name):]: var
+            for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope.name)}
+    return state_out, q_vars
+
+def get_network(state_dim, action_dim, hidden_nodes=HIDDEN_NODES):
+    """Define the neural network used to approximate the q-function
+
+    The suggested structure is to have each output node represent a Q value for
+    one action. e.g. for cartpole there will be two output nodes.
+
+    1) Given how q-values are used within RL, is it necessary to have output
+    activation functions?
+    2) You will set `target_in` in `get_train_batch` further down. Probably best
+    to implement that before implementing the loss .from there on you can figure it out easily
+    """
+
+def get_train_batch(q, t,state_in, action_in, target_in,replay_buffer):
+    """
+    Generate Batch samples for training by sampling the replay buffer"
+    Batches values are suggested to be the following;
+        state_batch: Batch of state values
+        action_batch: Batch of action values
+        target_batch: Target batch for (s,a) pair i.e. one application
+            of the bellman update rule.
+
+    return:
+        target_batch, state_batch, action_batch
+
+    1) To calculate the target batch values, you will need to use the
+    q_values for the next_state for each entry in the batch.
+    2) The target value, combined with your loss defined in `get_network()`
+
+
+    """
+
+if __name__ == "__main__":
+    main()
